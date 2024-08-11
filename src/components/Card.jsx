d691c73234e2fbb1d3ed8672fad4b8a0d3de5bda
neoqasim <neoqasim@gmail.com>
2024-08-11 13:46:56 +0500

    compeleted the minmilastic project

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,33 @@
 import { MdOutlineFileDownload } from "react-icons/md";
+import { motion } from "framer-motion"
 
 
-const Card = () => {
+const Card = ({ data, dragRef }) => {
+
     return (
         <>
-            <div className="relative h-80 w-64 rounded-[2rem] bg-zinc-900/85 text-white px-6 py-8 overflow-hidden " >
-                <p className="text-sm leading-tight mt-4 font-semibold" >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit qui architecto, blanditiis quod deleniti ipsum?</p>
+            <motion.div
+                whileDrag={{ scale: "0.99" }}
+                whileTap={{ scale: 1.1 }}
+                dragTransition={{ bounceStiffness: 1000, bounceDamping: 30 }}
+                dragConstraints={dragRef}
+
+                drag
+
+                className="relative h-80 w-64 rounded-[2rem] bg-zinc-900/85 text-white px-6 py-8 overflow-hidden " >
+                <p className="text-base  leading-tight mt-4 " >{data.desc}</p>
                 <div className="footer absolute bottom-0  left-0 w-full py-12  ">
                     <div className="flex justify-between py-8 px-8 items-center" >
-                        <h3>0.43mb</h3>
-                        <span className="bg-zinc-700 p-1 rounded-full" ><MdOutlineFileDownload size={20} /></span>
+                        <h3>{data.fileSize}</h3>
+                        <span className="bg-zinc-700 rounded-full text-lg h-7 w-7 flex justify-center items-center " >{data.close ? (<MdOutlineFileDownload size={24} />) : (<h1 className="mb-1" >x</h1>)}</span>
                     </div>
-                    <div className="w-full h-12 absolute flex justify-center items-center bg-indigo-600 ">
-                        <h3 className="text-sm font-semibold text-center ">Download now</h3>
-                    </div>
+                    {data.tag.isOpen &&
+                        <div className={`w-full h-12 absolute flex justify-center items-center `} style={{ background: data.tag.tagColor, }}>
+                            <h3 style={{ mixBlendMode: "difference" }} className=" font-semibold text-center text-yellow-400 ">Download now</h3>
+                        </div>
+                    }
                 </div>
-            </div>
+            </motion.div>
 
         </>
     )
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,15 +1,37 @@
+import { useRef } from "react"
 import Card from "./Card"
 
 const Foreground = () => {
-    // const [first, setfirst] = useState(second)
+    const ref = useRef(null)
     const data = [
-
+        {
+            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et suscipit iste impedit commodi aut.",
+            fileSize: "0.9mbs",
+            close: true,
+            tag: { isOpen: true, tagTitle: "Download now", tagColor: "blue" }
+        },
+        {
+            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et suscipit iste impedit commodi aut.",
+            fileSize: "0.9mbs",
+            close: true,
+            tag: { isOpen: true, tagTitle: "Download now", tagColor: "green" }
+        },
+        {
+            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et suscipit iste impedit commodi aut.",
+            fileSize: "0.9mbs",
+            close: true,
+            tag: { isOpen: true, tagTitle: "Download now", tagColor: "yellow" }
+        },
     ]
 
     return (
         <>
-            <div className="fixed top-0 left-0 z-[3] w-full h-full " >
-                <Card />
+            <div ref={ref} className="fixed top-0 left-0 z-[3] w-full h-full flex gap-12 flex-wrap flex-shrink " >
+                {
+                    data.map((item, index) => (
+                        <Card dragRef={ref} key={index} data={item} />
+                    ))
+                }
             </div>
         </>
 

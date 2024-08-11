bdf817ac60e4987ebdd127bbbb48277eda2da36e
neoqasim <neoqasim@gmail.com>
2024-08-11 22:43:27 +0500

    bg changes

--- src/components/Bg.jsx
+++ src/components/Bg.jsx
@@ -3,7 +3,7 @@ const Bg = () => {
     return (
         <>
             <div className=" absolute top-[4%] w-full text-zinc-500 font-semibold text-xl text-center py-10  ">Documents..</div>
-            <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[10vw] leading-none tracking-tighter font-serif font-semibold text-zinc-900">Docs. </h1>
+            <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[10vw] leading-none tracking-[-0.15em] font-ser if font-semibold text-zinc-900">dragDocs. </h1>
         </>
     )
 }
